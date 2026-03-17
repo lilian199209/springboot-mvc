48df830ba0c5eaa2ef66a8c079dfaa36c6fd967f
lilia <lilia@MSI>
2026-03-17 22:44:59 +0800

    3/17 upload data

--- src/main/java/com/example/demo/controller/ApiController.java
+++ src/main/java/com/example/demo/controller/ApiController.java
@@ -1,12 +1,20 @@
 package com.example.demo.controller;
 
+import java.lang.StackWalker.Option;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.BMI;
+import com.example.demo.model.Book;
 import com.example.demo.response.ApiResponse;
 
 // @Controller
@@ -120,6 +128,177 @@ public class ApiController {
 		//ApiResponse<BMI> apiResponse = new ApiResponse<BMI>("計算成功", bmi);
 		//return ResponseEntity.ok(apiResponse);
 		return ResponseEntity.ok(ApiResponse.success("計算成功", bmi));
+	}
+	
+	/**
+	 * 6. 同名多筆資料
+	 * 路徑: /json/age?age=170&age=60&age=4
+	 * 網址: http://localhost:8080/api/json/age?age=17&age=21&age=20
+	 * 請計算出平均年齡
+	 */
+	@GetMapping(value = "/json/age", produces = "application/json;charset=utf-8")
+	public ResponseEntity<ApiResponse<Object>> getAverage(@RequestParam(value = "age", required = false) List<Integer> ages) {
+		if(ages == null || ages.size() == 0) {
+			return ResponseEntity.badRequest().body(ApiResponse.error("請輸入年齡(age)"));
+		}
+		
+		double avg = ages.stream().mapToInt(Integer::valueOf).average().orElseGet(() -> 0);
+		Object data = Map.of("年齡", ages, "平均年齡", String.format("%.1f", avg));
+		return ResponseEntity.ok(ApiResponse.success("計算成功", data));
+	}
+	
+	/**
+	 * 7. Lab 練習: 得到多筆 score 資料
+	 * 路徑: "/json/score?score=80&score=100&score=50&score=70&score=30"
+	 * 網址: http://localhost:8080/api/json/score?score=80&score=100&score=50&score=70&score=30
+	 * 請自行設計一個方法，此方法可以
+	 * 印出: 最高分=?、最低分=?、平均=?、總分=?、及格分數列出=?、不及格分數列出=?
+	 */
+	@GetMapping(value = "/json/score", produces = "application/json;charset=utf-8")
+	public ResponseEntity<ApiResponse<Object>> getScoreAverage(@RequestParam(value = "score", required = false) List<Integer> scores) {
+		if(scores == null || scores.size() == 0) {
+			return ResponseEntity.badRequest().body(ApiResponse.error("請輸入分數(score)"));
+		}
+		
+		double avg = scores.stream().mapToInt(Integer::valueOf).average().orElseGet(() -> 0);
+		double max = scores.stream().mapToInt(Integer::valueOf).max().orElseGet(() -> 0);
+		double min = scores.stream().mapToInt(Integer::valueOf).min().orElseGet(() -> 0);
+		double sum = scores.stream().mapToInt(Integer::valueOf).sum();
+		
+		List good = new ArrayList<>();
+		List bad = new ArrayList<>();
+		for(Integer s:scores) {
+			if(s >= 60) {
+				good.add(s);
+			}else {
+				bad.add(s);
+			}
+		}
+		
+		Object data = Map.of("最高分", max, "最低分", min, "平均", String.format("%.1f", avg), "總分", sum, "及格分數", good, "不及格分數", bad);
+		return ResponseEntity.ok(ApiResponse.success("計算成功", data));
+	}
+	
+	/**
+	 * 8. 多筆參數轉 Map
+	 * name 書名(String), price 價格(Double), amount 數量(Integer), pub 出刊/停刊(Boolean)
+	 * 路徑: /json/book?name=Math&price=12.5&amount=10&pub=true
+	 * 路徑: /json/book?name=English&price=10.5&amount=20&pub=false
+	 * 網址: http://localhost:8080/api/json/book?name=Math&price=12.5&amount=10&pub=true
+	 * 網址: http://localhost:8080/api/json/book?name=English&price=10.5&amount=20&pub=false
+	 * 讓參數自動轉成 key/value 的 Map 集合
+	 */
+	@GetMapping(value = "/json/book", produces = "application/json;charset=utf-8")
+	public ResponseEntity<ApiResponse<Object>> getBookInfo(@RequestParam Map<String, Object> bookMap) {
+		System.out.printf("bookMap = %s%n", bookMap);
+		return ResponseEntity.ok(ApiResponse.success("成功", bookMap));
+	}
+	
+	/**
+	 * 9. 多筆參數轉 model
+	 * 路徑: /json/book2?name=Math&price=12.5&amount=10&pub=true
+	 * 網址: http://localhost:8080/api/json/book2?name=Math&price=12.5&amount=10&pub=true
+	 */
+	@GetMapping(value = "/json/book2", produces = "application/json;charset=utf-8")
+	public ResponseEntity<ApiResponse<Object>> getBookInfo2(Book book) {
+		book.setId(1); // 設定id
+		System.out.printf("book = %s%n", book);
+		return ResponseEntity.ok(ApiResponse.success("成功", book));
+	}
+	
+	/**
+	 * 10. 路徑參數
+	 * 
+	 * 早期設計風格
+	 * 路徑: /json/book?id=1 得到 id=1 的書
+	 * 路徑: /json/book?id=2 得到 id=2 的書
+	 * 
+	 * 現代設計風格(REST)
+	 * GET /books  查詢所有書籍
+	 * GET /book/1 查詢指定書籍
+	 * 
+	 * 路徑: /json/book/1 得到 id=1 的書
+	 * 路徑: /json/book/2 得到 id=2 的書
+	 * 網址: http://localhost:8080/api/json/book/1
+	 * 網址: http://localhost:8080/api/json/book/2
+	 */
+	@GetMapping(value = "/json/book/{id}")
+	//public ResponseEntity<ApiResponse<Book>> getBookById(@PathVariable(name = "id") Integer id) {
+	public ResponseEntity<ApiResponse<Book>> getBookById(@PathVariable Integer id) {
+		// 書庫
+		List<Book> books = List.of(
+				new Book(1, "多拉A夢", 12.5, 20, true),
+				new Book(2, "蠟筆小新", 10.5, 30, true),
+				new Book(3, "名偵探柯南", 9.5, 40, true),
+				new Book(4, "多拉B夢", 14.5, 0, false));
+		
+		// 根據id搜尋該筆書籍
+		Optional<Book> optBook = books.stream().filter(book -> book.getId().equals(id)).findFirst();
+		
+		// 判斷是否有找到
+		if(optBook.isEmpty()) {
+			return ResponseEntity.badRequest().body(ApiResponse.error("查無此書"));
+		}
+		
+		Book book = optBook.get(); // 取得書籍物件
+		return ResponseEntity.ok(ApiResponse.success("查詢成功", book));
+		
+	}
+	
+	/**
+	 * 書庫: 請參考上面的實作
+	 * 
+	 * 得到已出版(pub=true)的書籍
+	 * 網址: http://localhost:8080/api/json/book/pub/true
+	 * 
+	 * 得到未出版(pub=false)的書籍
+	 * 網址: http://localhost:8080/api/json/book/pub/false
+	 */
+	@GetMapping(value = "/json/book/pub/{pub}")
+	public ResponseEntity<ApiResponse<List<Book>>> getBookByPub(@PathVariable Boolean pub) {
+		// 書庫
+		List<Book> books = List.of(
+				new Book(1, "多拉A夢", 12.5, 20, true),
+				new Book(2, "蠟筆小新", 10.5, 30, true),
+				new Book(3, "名偵探柯南", 9.5, 40, true),
+				new Book(4, "多拉B夢", 14.5, 0, false));
+		
+		// 根據id搜尋該筆書籍
+		List<Book> optBook = new ArrayList<Book>();
+		for(Book book : books) {
+			if(book.getPub().equals(pub)) {
+				optBook.add(book);
+			}
+		}
+		
+		// 判斷是否有找到
+		if(optBook.isEmpty()) {
+			return ResponseEntity.badRequest().body(ApiResponse.error("查無資料"));
+		}
+		
+		return ResponseEntity.ok(ApiResponse.success("查詢成功", optBook));
+		
+	}
+	
+	@GetMapping(value = "/json/book/pub2/{pub}")
+	public ResponseEntity<ApiResponse<List<Book>>> getBookByPub2(@PathVariable Boolean pub) {
+		// 書庫
+		List<Book> books = List.of(
+				new Book(1, "多拉A夢", 12.5, 20, true),
+				new Book(2, "蠟筆小新", 10.5, 30, true),
+				new Book(3, "名偵探柯南", 9.5, 40, true),
+				new Book(4, "多拉B夢", 14.5, 0, false));
+		
+		// 根據id搜尋該筆書籍
+		List<Book> optBook = books.stream().filter(book -> book.getPub().equals(pub)).toList();
+		
+		// 判斷是否有找到
+		if(optBook.size() == 0) {
+			return ResponseEntity.badRequest().body(ApiResponse.error("查無" + (pub?"出刊":"停刊") + "書籍資料"));
+		}
+		
+		return ResponseEntity.ok(ApiResponse.success("查詢成功:" + (pub?"出刊":"停刊"), optBook));
+		
 	}
 
 }
---
+++ src/main/java/com/example/demo/repository/BookRepositoryImpl.java
@@ -0,0 +1,74 @@
+package com.example.demo.repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.Book;
+
+@Repository // 專門負責 資料存取 的元件, Spring 會自動建立該物件並管理
+public class BookRepositoryImpl implements BookRepository {
+	// InMemory 版
+	private List<Book> books = new CopyOnWriteArrayList<>();
+	
+	// 初始資料有4本書
+	{
+		books.add(new Book(1, "多拉A夢", 12.5, 20, true));
+		books.add(new Book(2, "蠟筆小新", 10.5, 30, true));
+		books.add(new Book(3, "名偵探柯南", 9.5, 40, true));
+		books.add(new Book(4, "多拉B夢", 14.5, 0, false));
+	}
+
+	@Override
+	public List<Book> findAllBooks() {
+		return books;
+	}
+
+	@Override
+	public Optional<Book> getBookById(Integer id) {
+		return books.stream().filter(book -> book.getId().equals(id)).findFirst();
+	}
+
+	@Override
+	public boolean addBook(Book book) {
+		// 建立 newId
+		OptionalInt optMaxId = books.stream().mapToInt(Book::getId).max();
+		int newId = optMaxId.isEmpty() ? 1 : optMaxId.getAsInt() + 1;
+		// 將 newId 設定給 book
+		book.setId(newId);
+		return books.add(book);
+	}
+
+	@Override
+	public boolean updateBook(Integer id, Book book) {
+		// 透過 id 找到要修改的書
+		Optional<Book> optBook = getBookById(id);
+		if(optBook.isEmpty()) {
+			return false;
+		}
+		// 得到要修改的書
+		Book orginalBook = optBook.get();
+		// 更新欄位資料
+		orginalBook.setAmount(book.getAmount());
+		orginalBook.setName(book.getName());
+		orginalBook.setPrice(book.getPrice());
+		orginalBook.setPub(book.getPub());
+		
+		return true;
+	}
+
+	@Override
+	public boolean deleteBook(Integer id) {
+		// 透過 id 找到要刪除的書
+		Optional<Book> optBook = getBookById(id);
+		if(optBook.isEmpty()) {
+			return false;
+		}
+		// 移除書籍
+		return books.remove(optBook.get());
+	}
+
+}
